a4cebb5dd53ffb797187bf80975409ace2c5323d
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2013-06-18 00:40:41 +0900

    Added incr command for strings

--- test/redis-mock.strings.test.js
+++ test/redis-mock.strings.test.js
@@ -44,3 +44,53 @@ describe("get", function () {
 
 });
 
+describe("incr", function () {
+
+    it("should increment number", function(done) {
+
+        var r = redismock.createClient("", "", "");
+
+        r.set("foo", "10", function (err, result) {
+
+            r.incr("foo", function (err, result) {
+
+                result.should.eql(11);
+
+                r.get("foo", function (err, result) {
+
+                    result.should.eql("11");
+
+                    done();
+
+                });
+
+            });
+
+        });
+
+    });
+
+
+    it("should set 0 before performing if key does not exist", function(done) {
+
+        var r = redismock.createClient("", "", "");
+
+        r.incr("bar", function (err, result) {
+
+            result.should.eql(1);
+
+            r.get("bar", function (err, result) {
+
+                console.log(typeof result);
+
+                result.should.eql("1");
+
+                done();
+
+            });
+
+        });
+
+    });
+
+});
