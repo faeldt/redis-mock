ab45f2a83a693f4a461821b919cda696dc32ced3
Kristian Faeldt <kristian.faeldt@gmail.com>
2015-01-06 00:18:57 +0900

    Fixed strings test that timed out when using actual timers

--- test/timer-helper.js
+++ test/timer-helper.js
@@ -1,4 +1,5 @@
 if (process.env['VALID_TESTS']) {
+  // Sinon will not work against actual redis so replace instead just return an noop interface
   module.exports = {
 	useFakeTimers: function() {
 		return {
